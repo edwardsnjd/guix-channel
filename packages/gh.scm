15bece21808e2f15cc096b9c8628e3d23fe7ddef
Nicholas Edwards <edwardsnjd@googlemail.com>
2025-11-02 14:05:56 +0800

    chore: Bump version of gh

--- packages/gh.scm
+++ packages/gh.scm
@@ -7,13 +7,17 @@
 (define-public gh
   (package
     (name "gh")
-    (version "2.65.0")
+    (version "2.82.1")
     (source (origin
               (method url-fetch)
               (uri (string-append "https://github.com/cli/cli/releases/download/v" version "/gh_" version "_linux_amd64.tar.gz"))
-              (sha256 "0nq80kmcpnvgmh1qbnhnrvn9q6nfzrg9j1pb3xyjn245wzpnj9bn")))
+              (sha256 "0as6z9bwf0prjx35rmzrany1pqxkj3lqmhkc9qwfmk6zfs3aibdg")))
     (build-system copy-build-system)
     (home-page "https://cli.github.com/")
     (synopsis "GitHub’s official command line tool")
     (description "gh is GitHub on the command line. It brings pull requests, issues, and other GitHub concepts to the terminal next to where you are already working with git and your code.")
     (license license:expat)))
+
+;; This allows you to run guix shell -f kotlin.scm
+;; Remove this line if you just want to define a package.
+gh
